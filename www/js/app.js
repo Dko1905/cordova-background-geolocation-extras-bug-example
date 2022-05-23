7e36e66d4754b1eb8afabcb7a27b72ab920809bc
Daniel Florescu <df@z5.lt>
2022-05-23 10:54:51 +0200

    Initial code

--- www/js/app.js
+++ www/js/app.js
@@ -2,25 +2,81 @@ const app = angular.module('app-template', ['onsen']);
 
 app.controller('PageController', function($scope) {
 	const geo = window.BackgroundGeolocation;
+	$scope.status = "not ready";
+	let priv = {};
 
-	$scope.ready = function() {
-		geo.ready({
-			reset: true,
-			url: 'http://192.168.1.44',
-			autoSync: false,
-			batchSync: true
-		})
-			.then(_ => console.log('READY'))
-			.catch(err => console.error(Error(err)));
+	priv.config = {
+		desiredAccuracy: geo.DESIRED_ACCURACY_HIGH,
+		reset: false,
+		stopOnTerminate: false,
+		startOnBoot: false,
+		extras: {routeId: "default"}
 	};
 
-	$scope.test = function() {
-		geo.resetOdometer() // <-- Bug right here
-			.then(location => {
-				console.log(JSON.stringify(location));
-				console.log(location.timestamp); // <-- Incorrect output
-				console.log(location.timestamp instanceof Date)
-				console.log(new Date() instanceof Date) // Just to be sure
+	$scope.start = async function() {
+		let state = await geo.ready(priv.config);
+		console.log(`>> Ready ${state.enabled} ${JSON.stringify(state.extras)}`);
+		$scope.listenToLocation();
+		$scope.changeRouteId();
+		await geo.start();
+		console.log('>> Started');
+		await geo.changePace(true);
+		console.log('>> Changed pace');
+	};
+
+	$scope.pause = async function() {
+		$scope.changeRouteId();
+		$scope.insertLocation();
+	}
+
+	$scope.continue = async function() {
+		let state = await geo.ready(); // Intentionally empty
+		console.log(`>> Ready ${state.enabled} ${JSON.stringify(state.extras)}`);
+		$scope.listenToLocation();
+	};
+
+	$scope.stop = async function() {
+		let state = await geo.ready(); // Intentionally empty
+		console.log(`>> Ready ${state.enabled} ${JSON.stringify(state.extras)}`);
+		$scope.listenToLocation();
+		await geo.stop();
+	};
+
+	$scope.listenToLocation = function() {
+		geo.onLocation(loc => {
+			console.debug(`onLocation: ${loc.timestamp.toLocaleTimeString()} ${JSON.stringify(loc.extras)}`);
+		});
+		console.log('>> Listening to onLocation');
+	};
+
+	$scope.changeRouteId = async function() {
+		priv.config.extras.routeId = uuid.v4();
+		await geo.reset(priv.config);
+		console.log(`>> Update route ID: ${priv.config.extras.routeId}`);
+	};
+
+	$scope.insertLocation = async function() {
+		const timestamp = new Date();
+		const liteGeolocationLocation = {
+			timestamp: timestamp,
+			coords: {
+				latitude: 1,
+				longitude: 2,
+				altitude: 3
+			},
+			extras: priv.config.extras
+		};
+		await geo.insertLocation(liteGeolocationLocation);
+		console.log('>> Inserted location');
+	};
+
+	$scope.printLocations = function() {
+		return geo.getLocations()
+			.then(locations => {
+				console.debug('> Locations')
+				for (const loc of locations) {
+					console.debug(`\t${loc.timestamp.toLocaleTimeString()} ${JSON.stringify(loc.extras)}`);
+				}
 			})
 			.catch(err => console.error(Error(err)));
 	};
